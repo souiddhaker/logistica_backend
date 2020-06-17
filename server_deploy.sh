87e734bec3887a3c12e67af7f7df658e0edc58de
mohamed amine hamza <medaminehamza7@gmail.com>
2020-06-17 14:07:24 +0100

    update deploy

--- server_deploy.sh
+++ server_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-composer install
-php artisan migrate migrate:fresh --seed
+/opt/plesk/php/7.3/bin/php /usr/lib/plesk-9.0/composer.phar install
+/opt/plesk/php/7.3/bin/php artisan migrate migrate:fresh --seed
 chown -R dhaker:psaserv *
