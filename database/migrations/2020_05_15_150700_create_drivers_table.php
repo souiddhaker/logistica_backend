1186ab65c09c1999b9451cd797a835cca8573395
mohamed amine hamza <medaminehamza7@gmail.com>
2020-06-17 16:56:21 +0100

    fix delete option

--- database/migrations/2020_05_15_150700_create_drivers_table.php
+++ database/migrations/2020_05_15_150700_create_drivers_table.php
@@ -24,6 +24,7 @@ class CreateDriversTable extends Migration
                 ->onDelete('cascade');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->softDeletes();
         });
     }
 
