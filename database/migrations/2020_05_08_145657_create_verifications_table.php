60c94abb60852131233c72b3b53dfc37c3d9bf2f
mohamed amine hamza <medaminehamza7@gmail.com>
2020-06-17 13:46:11 +0100

    update result get Admin

--- database/migrations/2020_05_08_145657_create_verifications_table.php
+++ database/migrations/2020_05_08_145657_create_verifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVerificationsTable extends Migration
+class CreateVerifications2Table extends Migration
 {
     /**
      * Run the migrations.
