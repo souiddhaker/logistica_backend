edd9bbc80260a1f7e51ad31a0fe4d23b20fdbddb
Dhaker <souid.dhaker@gmail.com>
2020-06-19 14:19:55 +0100

    Update DocumentController.php

--- app/Http/Controllers/Api/DocumentController.php
+++ app/Http/Controllers/Api/DocumentController.php
@@ -76,13 +76,16 @@ class DocumentController extends Controller
                 if ($user->getRoles() === json_encode(['captain']))
                 {
                     $position  = strpos($document->path,'img/profile/',0);
-                    $image_path = public_path('img/profile/').substr($document->path,$position+12,strlen($document->path));
+//                    $image_path = public_path('img/profile/').substr($document->path,$position+12,strlen($document->path));
+                    $image_path = $document->path;
                 }else{
                     $position  = strpos($document->path,'img/attachement/',0);
-                    $image_path = public_path('img/attachement/').substr($document->path,$position+16,strlen($document->path));
+//                    $image_path = public_path('img/attachement/').substr($document->path,$position+16,strlen($document->path));
+                    $image_path = $document->path;
+
                 }
                 str_replace("//","/",$image_path);
-                return response()->json($image_path,200);
+//                return response()->json($image_path,200);
 
                 unlink($image_path);
                 $res->success();
