3d26efd9631ed3cfdd8186ba2a937d69c6cdeaa4
mohamed amine hamza <medaminehamza7@gmail.com>
2020-07-01 16:04:19 +0100

    update get user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request)
@@ -52,7 +52,7 @@ class UserController extends Controller
             $name = time() . '.' . explode('/', explode(':', substr($request->photo, 0, strpos($request->photo, ';')))[1])[1];
 
             $img = \Image::make($request->photo)->save(public_path('img/profile/') . $name);
-            $name = url('/') .'/img/profile/' . $name;
+            $name = url('/') . '/img/profile/' . $name;
 
             $user = Auth::user();
             $user->update(['image_url' => $name]);
@@ -76,20 +76,20 @@ class UserController extends Controller
         $res = new Result();
 
         $user = Auth::user();
-        dd($user['id'],$user->get('id'),User::where('id', $user[0]['id'])->limit(1));
-        $data = User::where('id', $user[0]['id'])->limit(1)->get();
-        $dataRoles = AdminRoles::where('user_id',$user[0]['id'])->first()->get('roles');
-        if (count($dataRoles) > 0) {
-            $data[0]['adminRoles'] = $dataRoles[0]['roles'];
+        $dataRolesCount = AdminRoles::where('user_id', $user['id'])->count();
+        if ($dataRolesCount > 0) {
+            $dataRoles = AdminRoles::where('user_id', $user['id'])->first();
+            if ($dataRoles) {
+                $dataRoles = $dataRoles->get('roles');
+                $user['adminRoles'] = $dataRoles[0]['roles'];
+            } else {
+                $user['adminRoles'] = [];
+            }
         }
-        if($data[0]['roles']===json_encode(['admin'])){
-            $res->success($data);
-        }else{
-            $res->success($user);
-        }
-        $res->message= trans('messages.user_details');
+        $res->success($user);
+        $res->message = trans('messages.user_details');
 
-        return response()->json($res,200);
+        return response()->json($res, 200);
     }
 
     public function createAccount(int $id)
@@ -101,8 +101,8 @@ class UserController extends Controller
     }
 
 
-
-    public function userFcmToken(Request $request){
+    public function userFcmToken(Request $request)
+    {
 
         $res = new Result();
 
@@ -115,14 +115,13 @@ class UserController extends Controller
             return response()->json($res, 200);
         }
         $user = Auth::user();
-        if ($user)
-        {
+        if ($user) {
             $fcm = new UserFcm();
             $fcm->user_id = $user->id;
             $fcm->token = $request['fcm'];
             $fcm->save();
             $res->success($fcm);
-        }else{
+        } else {
             $res->fail('User Not Found');
         }
         return response()->json($res, 200);
@@ -132,14 +131,14 @@ class UserController extends Controller
 
     public function notify(Request $request)
     {
-        $notification_payload   = $request['payload'];
-        $notification_title     = $request['title'];
-        $notification_message   = $request['message'];
-        $receiver_id =[];
+        $notification_payload = $request['payload'];
+        $notification_title = $request['title'];
+        $notification_message = $request['message'];
+        $receiver_id = [];
 //        return [UserFcm::where('user_id',$request['user_id'])->first()];
-        $users = [UserFcm::where('user_id',$request['user_id'])->first()];
-        foreach ($users as $user){
-            array_push($receiver_id,$user['token']);
+        $users = [UserFcm::where('user_id', $request['user_id'])->first()];
+        foreach ($users as $user) {
+            array_push($receiver_id, $user['token']);
         }
 //        return $receiver_id;
 //        if (isset($request['user_id'])){
@@ -167,14 +166,14 @@ class UserController extends Controller
 
             $firebase = new Firebase();
 
-            $message = array('body' =>  $notification_message , 'title' => $notification_title , 'vibrate' => 1, 'sound' => 1 ,'payload'=>$notification_payload);
+            $message = array('body' => $notification_message, 'title' => $notification_title, 'vibrate' => 1, 'sound' => 1, 'payload' => $notification_payload);
 
             $response = '';
 
-            $response = $firebase->sendMultiple(  $receiver_id,  $message );
+            $response = $firebase->sendMultiple($receiver_id, $message);
 
-           return $response;
-        } catch ( \Exception $ex ) {
+            return $response;
+        } catch (\Exception $ex) {
             return false;
         }
     }
