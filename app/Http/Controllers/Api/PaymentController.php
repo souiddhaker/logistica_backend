c00dcbd6f32308ef7e52cf9fa7ba6a38fb90a2e4
Feker Hassine <faker@wi-mobi.com>
2020-09-04 12:41:30 +0100

    check hyperpay response

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -45,6 +45,8 @@ class PaymentController extends Controller
         }
         $hyperPayApi = new Hyperpay();
         $response = $hyperPayApi->getPaymentStatus($params);
+        return response()->json($response,200);
+
         if (isset($response["result"]["code"]) && ($response["result"]["code"] == "000.000.000" || $response["result"]["code"] == "000.100.112") )
         {
             $res->success = true;
